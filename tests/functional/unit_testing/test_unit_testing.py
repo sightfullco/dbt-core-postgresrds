87d2e40e547a96d6fff6e41ad3d0106ad61aef48
Gerda Shank <gerda@dbtlabs.com>
2024-03-12 14:01:50 -0400

    Add test for --exclude-resource-types unit_test

--- tests/functional/unit_testing/test_unit_testing.py
+++ tests/functional/unit_testing/test_unit_testing.py
@@ -59,6 +59,13 @@ class TestUnitTests:
             if result.node.unique_id == "model.test.my_model":
                 result.status == NodeStatus.Skipped
 
+        # Run build command but specify no unit tests
+        results = run_dbt(
+            ["build", "--select", "my_model", "--exclude-resource-types", "unit_test"],
+            expect_pass=True,
+        )
+        assert len(results) == 1
+
         # Test select by test name
         results = run_dbt(["test", "--select", "test_name:test_my_model_string_concat"])
         assert len(results) == 1
